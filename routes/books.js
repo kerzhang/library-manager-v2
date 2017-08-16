d9f1736b369dc51184fd683e970606f680b2e9dd
Ker Zhang <kerzhang@live.com>
2017-08-17 00:18:24 +0800

    pagination partially done

--- routes/books.js
+++ routes/books.js
@@ -2,97 +2,158 @@ var express = require('express');
 var router = express.Router();
 var db = require('../config/db.js');
 
+var PAGE_LIMIT = 6;
+var paginations = [];
+
+function countTotalPagesAndBooks(allBooks) {
+  paginations = [];
+  var booksCount = allBooks.length;
+  var pages = Math.ceil(booksCount / PAGE_LIMIT);
+  for (var index = 1; index <= pages; index++) {
+    paginations.push(index);  
+  }
+  console.log('Total ' + booksCount + ' books in ' + paginations + ' pages.');
+  console.log(paginations);
+}
+
 /* GET all books */
 router.get('/', function(req, res) {
   var today = new Date().toISOString().substring(0, 10);
 
-  if (req.query.filter === 'overdue') {
-    db.Book
-      .findAll({
-        include: {
-          model: db.Loan,
-          where: {
-            return_by: { $lt: today }
-          }
+  db.Book
+    .findAll()
+    .then(function(books) {
+      countTotalPagesAndBooks(books);
+    })
+    .then(function() {
+      if (req.query.filter === 'overdue') {
+        db.Book
+          .findAll({
+            include: {
+              model: db.Loan,
+              where: {
+                return_by: { $lt: today }
+              }
+            }
+          })
+          .then(function(books) {
+            console.log(books);
+            res.render('overdue_books', {
+              books: books,
+              pageTitle: 'Overdue Books'
+            });
+          })
+          .catch(function(error) {
+            console.log(error);
+            res.status(500).send(error);
+          });
+        // } else {
+        //   res.sendStatus(404);
+      } else if (req.query.filter === 'checked_out') {
+        db.Book
+          .findAll({
+            include: {
+              model: db.Loan,
+              where: {
+                returned_on: null
+              }
+            }
+          })
+          .then(function(books) {
+            console.log(books);
+            res.render('checked_books', {
+              books: books,
+              pageTitle: 'Books Not Returned'
+            });
+          })
+          .catch(function(error) {
+            res.status(500).send(error);
+          });
+      } else if (req.query.page !== undefined) {
+        let clickedPage = 1;
+        if (req.query.page <= paginations.length && req.query.page > 0) {
+          console.log('current page is: ' + req.query.page);
+          clickedPage = req.query.page;
+        db.Book
+          .findAll({
+            order: [['title', 'DESC']],
+            limit: PAGE_LIMIT,
+            offset: PAGE_LIMIT * (parseInt(req.query.page)-1),
+          })
+          .then(function(books) {
+            res.render('all_books', { 
+              books: books, 
+              pageTitle: 'Book List',
+              currentPage: clickedPage,
+              totalPages: paginations            
+            });
+          })
+          .catch(function(error) {
+            console.log('Error: ' + error);
+            res.status(500).send(error);
+          })
+        } else {
+          var err = new Error('That page doesn\'t exist!');
+          err.status = 404;
+          res.render('error', {
+            errors: [err],
+            path: '/books/'
+          })
         }
-      })
-      .then(function(books) {
-        console.log(books);
-        res.render('overdue_books', {
-          books: books,
-          pageTitle: 'Overdue Books'
-        });
-      })
-      .catch(function(error) {
-        console.log(error);
-        res.status(500).send(error);
-      });
-    // } else {
-    //   res.sendStatus(404);
-  } else if (req.query.filter === 'checked_out') {
-    db.Book
-      .findAll({
-        include: {
-          model: db.Loan,
-          where: {
-            returned_on: null
-          }
-        }
-      })
-      .then(function(books) {
-        console.log(books);
-        res.render('checked_books', {
-          books: books,
-          pageTitle: 'Books Not Returned'
-        });
-      })
-      .catch(function(error) {
-        res.status(500).send(error);
-      });
-  } else {
-    db.Book
-      .findAll({
-        order: [['title', 'DESC']]
-      })
-      .then(function(books) {
-        res.render('all_books', { books: books, pageTitle: 'Book List' });
-      })
-      .catch(function(error) {
-        console.log("Error: " + error);
-        res.status(500).send(error);
-      });
-  }
+      } else {
+        db.Book
+          .findAll({
+            order: [['title', 'DESC']],
+            limit: PAGE_LIMIT,
+            offset: 0,
+          })
+          .then(function(books) {
+            res.render('all_books', { 
+              books: books, 
+              pageTitle: 'Book List',
+              currentPage: 1,
+              totalPages: paginations
+             });
+          })
+          .catch(function(error) {
+            console.log('Book Error: ' + error);
+            res.status(500).send(error);
+          });
+      }
+    });
 });
 
 //Save new book
 router.post('/', function(req, res, next) {
-  db.Book.create(req.body).then(function() {
-    res.redirect('/books');
-  }).catch(function(err) {
-    if(err.name === 'SequelizeValidationError') {
-      res.render('new_book', {
-        book: db.Book.build(req.body),
-        errors: err.errors
-      })
-    } else {
-      throw err;
-    }
-  }).catch(function(err) {
-    console.log("Error: " + err);
-    res.status(500).send(err);
-  });
+  db.Book
+    .create(req.body)
+    .then(function() {
+      res.redirect('/books');
+    })
+    .catch(function(err) {
+      if (err.name === 'SequelizeValidationError') {
+        res.render('new_book', {
+          book: db.Book.build(req.body),
+          errors: err.errors
+        });
+      } else {
+        throw err;
+      }
+    })
+    .catch(function(err) {
+      console.log('Error: ' + err);
+      res.status(500).send(err);
+    });
 });
-
 
 //Add new book
 router.get('/new', function(req, res, next) {
   res.render('new_book', { book: db.Book.build(), pageTitle: 'New Book' });
 });
 
-
 //Return book
 router.get('/:id/return', function(req, res, next) {
-  console.log('got the book ' + req.params.id);
+  console.log('Return the book: ' + req.params.id);
   db.Book
     .findById(req.params.id)
     .then(function(book) {
@@ -118,23 +179,24 @@ router.post('/:id', function(req, res, next) {
     })
     .then(function(book) {
       res.redirect('/books');
-    }).catch(function(err) {
-      if(err.name === 'SequelizeValidationError') {
+    })
+    .catch(function(err) {
+      if (err.name === 'SequelizeValidationError') {
         res.render('error', {
           errors: err.errors,
           path: '/books/'
-        })
+        });
       } else {
         throw err;
       }
-    }).catch(function(err) {
-      console.log("Error: " + err);
+    })
+    .catch(function(err) {
+      console.log('Error: ' + err);
       res.status(500).send(err);
     });
 });
 
-
-router.get('/:id', function(req, res) {
+router.get('/:id/', function(req, res) {
   db.Book.findById(req.params.id).then(function(book) {
     db.Loan
       .findAll({
--- routes/loans.js
+++ routes/loans.js
@@ -2,86 +2,148 @@ var express = require('express');
 var router = express.Router();
 var db = require('../config/db.js');
 
+var PAGE_LIMIT = 6;
+var paginations = [];
+
+function countTotalPagesAndLoans(allLoans) {
+  paginations = [];
+  var loansCount = allLoans.length;
+  var pages = Math.ceil(loansCount / PAGE_LIMIT);
+  for (var index = 1; index <= pages; index++) {
+    paginations.push(index);
+  }
+  console.log(paginations);
+}
+
 /* GET all loans record */
 router.get('/', function(req, res) {
+  var today = new Date().toISOString().substring(0, 10);
 
-  var today = new Date().toISOString().substring(0,10);
-  
-    if (req.query.filter === 'overdue') {
-      db.Loan
-        .findAll({
-          include: [
-            {
-              model: db.Book
-            },
-            {
-              model: db.Patron
-            }
-          ],
-          where: {
-            return_by : { 
-              $lt: today 
-            }
-          }
-        })
-        .then(function(loans) {
-          res.render('overdue_loans', {
-            loans: loans,
-            pageTitle: 'Overdue Loans'
-          });
-        })
-        .catch(function(error) {
-          console.log(error);
-          res.status(500).send(error);
-        });
-
-    } else if (req.query.filter === 'checked_out') {
-      db.Loan
-        .findAll({
-          include: [
-            {
-              model: db.Book
-            },
-            {
-              model: db.Patron
-            }
-          ],
-          where: {
-            returned_on: null
-          }
-        })
-        .then(function(loans) {
-          res.render('checked_loans', {
-            loans: loans,
-            pageTitle: 'Loans Checked Out'
-          });
-        })
-        .catch(function(error) {
-          res.status(500).send(error);
-        });
-    } else {
   db.Loan
-    .findAll({
-      include: [
-        {
-          model: db.Book
-        },
-        {
-          model: db.Patron
-        }
-      ]
-    })
+    .findAll()
     .then(function(loans) {
-      res.render('all_loans', {
-        loans: loans,
-        pageTitle: 'Loan Records'
-      });
-      // console.log(loans);
+      countTotalPagesAndLoans(loans);
     })
-    .catch(function(error) {
-      // console.log(error);
-      res.status(500).send(error);
-    });}
+    .then(function() {
+      if (req.query.filter === 'overdue') {
+        db.Loan
+          .findAll({
+            include: [
+              {
+                model: db.Book
+              },
+              {
+                model: db.Patron
+              }
+            ],
+            where: {
+              return_by: {
+                $lt: today
+              }
+            }
+          })
+          .then(function(loans) {
+            res.render('overdue_loans', {
+              loans: loans,
+              pageTitle: 'Overdue Loans'
+            });
+          })
+          .catch(function(error) {
+            console.log(error);
+            res.status(500).send(error);
+          });
+      } else if (req.query.filter === 'checked_out') {
+        db.Loan
+          .findAll({
+            include: [
+              {
+                model: db.Book
+              },
+              {
+                model: db.Patron
+              }
+            ],
+            where: {
+              returned_on: null
+            }
+          })
+          .then(function(loans) {
+            res.render('checked_loans', {
+              loans: loans,
+              pageTitle: 'Loans Checked Out'
+            });
+          })
+          .catch(function(error) {
+            res.status(500).send(error);
+          });
+      } else if (req.query.page !== undefined) {
+        let clickedPage = 1;
+        if (req.query.page <= paginations.length && req.query.page > 0) {
+          console.log('current page is: ' + req.query.page);
+          clickedPage = req.query.page;
+          db.Loan
+            .findAll({
+              include: [
+                {
+                  model: db.Book
+                },
+                {
+                  model: db.Patron
+                }
+              ],
+              limit: PAGE_LIMIT,
+              offset: PAGE_LIMIT * (parseInt(req.query.page) - 1)
+            })
+            .then(function(loans) {
+              res.render('all_loans', {
+                loans: loans,
+                pageTitle: 'Loan Records',
+                currentPage: clickedPage,
+                totalPages: paginations
+              });
+              // console.log(loans);
+            })
+            .catch(function(error) {
+              // console.log(error);
+              res.status(500).send(error);
+            });
+        } else {
+          var err = new Error('That page doesn\'t exist!');
+          err.status = 404;
+          res.render('error', {
+            errors: [err],
+            path: '/loans/'
+          })
+        }
+      } else {
+        db.Loan
+          .findAll({
+            include: [
+              {
+                model: db.Book
+              },
+              {
+                model: db.Patron
+              }
+            ],
+            limit: PAGE_LIMIT,
+            offset: 0,
+          })
+          .then(function(loans) {
+            res.render('all_loans', {
+              loans: loans,
+              pageTitle: 'Loan Records',
+              currentPage: 1,
+              totalPages: paginations
+            });
+            // console.log(loans);
+          })
+          .catch(function(error) {
+            console.log('Loan Error: ' + error);
+            res.status(500).send(error);
+          });
+      }
+    });
 });
 
 //Add new book
@@ -98,7 +160,9 @@ router.get('/new', function(req, res, next) {
       .then(function() {
         var loanDate = new Date().toISOString();
         var today = new Date();
-        var returnDate = (new Date(today.setDate(today.getDate() + 7))).toISOString();
+        var returnDate = new Date(
+          today.setDate(today.getDate() + 7)
+        ).toISOString();
         res.render('new_loan', {
           patron: db.Loan.build(),
           books: allBooks,
@@ -114,21 +178,25 @@ router.get('/new', function(req, res, next) {
 //Save new book
 router.post('/', function(req, res, next) {
   // console.log(req.body);
-  db.Loan.create(req.body).then(function() {
-    res.redirect('/loans');
-  }).catch(function(err) {
-    if(err.name === 'SequelizeValidationError') {
-      res.render('error', {
-        errors: err.errors,
-        path: '/loans/'
-      })
-    } else {
-      throw err;
-    }
-  }).catch(function(err) {
-    console.log("Error: " + err);
-    res.status(500).send(err);
-  });
+  db.Loan
+    .create(req.body)
+    .then(function() {
+      res.redirect('/loans');
+    })
+    .catch(function(err) {
+      if (err.name === 'SequelizeValidationError') {
+        res.render('error', {
+          errors: err.errors,
+          path: '/loans/'
+        });
+      } else {
+        throw err;
+      }
+    })
+    .catch(function(err) {
+      console.log('Error: ' + err);
+      res.status(500).send(err);
+    });
 });
 
 module.exports = router;
